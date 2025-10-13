cf9b56c731113326320b1e035aebbddbfa521c58
fry69 <142489379+fry69@users.noreply.github.com>
2025-10-13 19:43:48 +0200

    feat: snapshot

---
+++ lex.config.ts
@@ -0,0 +1,7 @@
+// file: lex.config.ts
+import { defineLexiconConfig } from "@atcute/lex-cli";
+
+export default defineLexiconConfig({
+  files: ["lexicons/**/*.json"],
+  outdir: "src/lexicons/",
+});
