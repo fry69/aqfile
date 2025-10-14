27147c6532dd9c295a9c3c7df9e15b9ace8da198
fry69 <142489379+fry69@users.noreply.github.com>
2025-10-14 18:42:00 +0200

    chore: release v0.1.3

--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,7 @@ import { calculateChecksum, getFileMetadata } from "./utils.ts";
 import type { NetAltqAqfile } from "./types.ts";
 
 /** The current version of aqfile */
-const VERSION = "0.1.2";
+const VERSION = "0.1.3";
 
 /**
  * Options for uploading a file to the PDS
